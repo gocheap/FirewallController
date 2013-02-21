20af9ad3c55a92160e51f08afb1ffdd94f9aebe6
Saer <saergakov@gmail.com>
2013-02-21 16:36:32 +1200

    Refactor OS version checking

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -3,24 +3,18 @@
 #include <Windows.h>
 #include "MainWindow.hpp"
 
-bool IsSupportedOS()
+int main(int argc, char** argv)
 {
     OSVERSIONINFO osInfo = {};
     osInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
     GetVersionEx(&osInfo);
-    return (osInfo.dwMajorVersion >= 6);
-}
 
-int main(int argc, char** argv)
-{
-    QApplication app(argc, argv);
-
-    if (!IsSupportedOS()) {
+    if (osInfo.dwMajorVersion >= 6) {
+        QApplication app(argc, argv);
+        MainWindow window;
+        return app.exec();
+    } else {
         QMessageBox::critical(0, "Fatal error", "This program runs only in Vindows Vista or newer");
-        QApplication::exit(1);
+        return 1;
     }
-
-    MainWindow window;
-
-    return app.exec();
 }
